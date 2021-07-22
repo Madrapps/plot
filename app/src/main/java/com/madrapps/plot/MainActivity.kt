4820f3b745924d1e5afc02b3d9dd37f0ee04a172
thsaravana <th.saravana@gmail.com>
2021-07-22 23:23:32 +0530

    Draw points

--- app/src/main/java/com/madrapps/plot/MainActivity.kt
+++ app/src/main/java/com/madrapps/plot/MainActivity.kt
@@ -21,6 +21,25 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.madrapps.plot.ui.theme.PlotTheme
 
+private val dataPoints = listOf(
+    DataPoint(0f,0f),
+    DataPoint(1f,0f),
+    DataPoint(2f,0f),
+    DataPoint(3f,0f),
+    DataPoint(4f,0f),
+    DataPoint(5f,25f),
+    DataPoint(6f,75f),
+    DataPoint(7f,100f),
+    DataPoint(8f,80f),
+    DataPoint(9f,75f),
+    DataPoint(10f,55f),
+    DataPoint(11f,45f),
+    DataPoint(12f,50f),
+    DataPoint(13f,100f),
+    DataPoint(14f,75f),
+    DataPoint(15f,25f),
+)
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +47,7 @@ class MainActivity : ComponentActivity() {
             PlotTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LineGraph()
+                    LineGraph(dataPoints)
                 }
             }
         }
@@ -36,7 +55,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LineGraph() {
+fun LineGraph(dataPoints: List<DataPoint>) {
     val color = remember { mutableStateOf(Color.Gray) }
     val offset = remember {
         mutableStateOf(Offset(0f, 0f))
@@ -61,6 +80,15 @@ fun LineGraph() {
             }
         }, onDraw = {
         drawCircle(color.value, radius = 100f, center = offset.value)
+        val fX = size.width / dataPoints.size
+        val fY = size.height / dataPoints.maxOf { it.y }
+
+        var prevOffset: Offset? = null
+        dataPoints.forEach { (x,y) ->
+            val x1 = x * fX
+            val y1 = size.height - (y * fY)
+            drawCircle(Color.Blue, 10f, Offset(x1, y1))
+        }
     })
 
 }
@@ -69,6 +97,8 @@ fun LineGraph() {
 @Composable
 fun DefaultPreview() {
     PlotTheme {
-        LineGraph()
+        LineGraph(dataPoints)
     }
 }
+
+data class DataPoint(val x: Float, val y: Float)
--- app/src/main/java/com/madrapps/plot/ui/theme/Theme.kt
+++ app/src/main/java/com/madrapps/plot/ui/theme/Theme.kt
@@ -5,17 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = Color.White
 )
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = Color.White
 
     /* Other default colors to override
     background = Color.White,
