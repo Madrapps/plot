9bbde195e5205c8f606ef63c18ab8a8c9a12f614
thsaravana <th.saravana@gmail.com>
2021-07-23 19:01:13 +0530

    Changing long press timeout to 100L

--- app/src/main/java/com/madrapps/plot/MainActivity.kt
+++ app/src/main/java/com/madrapps/plot/MainActivity.kt
@@ -5,10 +5,8 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.rememberScrollableState
-import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,10 +18,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.madrapps.plot.ui.theme.Grey50
 import com.madrapps.plot.ui.theme.PlotTheme
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withTimeout
+import kotlin.coroutines.cancellation.CancellationException
 
 private val dataPoints = listOf(
     DataPoint(0f, 0f),
@@ -77,6 +79,7 @@ fun LineGraph(dataPoints: List<DataPoint>) {
     Canvas(modifier = Modifier
         .height(300.dp)
         .fillMaxWidth()
+        .background(Grey50)
         .scrollable(
             state = rememberScrollableState { delta ->
                 offset.value -= delta
@@ -88,8 +91,9 @@ fun LineGraph(dataPoints: List<DataPoint>) {
             }, Orientation.Horizontal, enabled = true,
             interactionSource = MutableInteractionSource()
         )
-        .pointerInput(Unit) {
-            detectDragGesturesAfterLongPress(
+        .pointerInput(Unit, Unit) {
+            Log.d("RONNY", "scope = $this")
+            detectDragGesturesAfterLongPress1(
                 onDragEnd = {
                     isDragging.value = false
                 }, onDragStart = {
@@ -98,9 +102,31 @@ fun LineGraph(dataPoints: List<DataPoint>) {
                 }, onDragCancel = {
                     isDragging.value = false
                 }) { change, _ ->
-                Log.d("RONNY", "change = $change")
                 dragOffset.value = change.position.x
             }
+//            forEachGesture {
+//                awaitPointerEventScope {
+//                    awaitFirstDown(requireUnconsumed = false)
+//                    do {
+//                        val event = awaitPointerEvent()
+//                        val canceled = event.changes.any { it.positionChangeConsumed() }
+//                        if (!canceled) {
+//                            val zoomChange = event.calculateZoom()
+//                        }
+//
+//                        if (event.changes.size == 1) {
+//                            Log.d("RONNY", "ONE")
+//                        } else if (event.changes.size == 2) {
+//                            Log.d("RONNY", "TWO")
+//                        } else {
+//                            Log.d("RONNY", "NONE")
+//                            break
+//                        }
+//                    } while (!canceled && event.changes.any { it.pressed })
+//                }
+//                Log.d("RONNY", "DONE")
+//            }
+
         },
         onDraw = {
             val xStart = 30.dp.toPx()
@@ -118,12 +144,17 @@ fun LineGraph(dataPoints: List<DataPoint>) {
             if (xLastPoint > availableWidth) {
                 maxScrollOffset.value = xLastPoint - availableWidth
             }
-
+            var xLock = 0f
             dataPoints.forEach { (x, y) ->
                 val x1 = (x * xOffset * xScale) + xStart - scrollOffset
                 val y1 = availableHeight - (y * yOffset * yScale)
                 val curOffset = Offset(x1, y1)
-                drawCircle(Color.Blue, pointRadius.toPx(), curOffset)
+                val color =
+                    if (isDragging.value && (dragOffset.value) > x1 - (xOffset * xScale) / 2 && (dragOffset.value) < x1 + (xOffset * xScale) / 2) {
+                        xLock = x1
+                        Color.Red
+                    } else Color.Blue
+                drawCircle(color, pointRadius.toPx(), curOffset)
                 if (prevOffset != null) {
                     drawLine(Color.Blue, prevOffset!!, curOffset, lineWidth.toPx())
                 }
@@ -131,8 +162,8 @@ fun LineGraph(dataPoints: List<DataPoint>) {
             }
             if (isDragging.value) {
                 drawLine(
-                    Color.Red, Offset(dragOffset.value, 0f),
-                    Offset(dragOffset.value, availableHeight), lineWidth.toPx()
+                    Color.Red, Offset(xLock, 0f),
+                    Offset(xLock, availableHeight), lineWidth.toPx()
                 )
             }
         })
@@ -147,3 +178,99 @@ fun DefaultPreview() {
 }
 
 data class DataPoint(val x: Float, val y: Float)
+
+suspend fun PointerInputScope.detectDragGesturesAfterLongPress1(
+    onDragStart: (Offset) -> Unit = { },
+    onDragEnd: () -> Unit = { },
+    onDragCancel: () -> Unit = { },
+    onDrag: (change: PointerInputChange, dragAmount: Offset) -> Unit
+) {
+    forEachGesture {
+        val down = awaitPointerEventScope {
+            awaitFirstDown(requireUnconsumed = false)
+        }
+        try {
+            val drag = awaitLongPressOrCancellation(down)
+            if (drag != null) {
+                onDragStart.invoke(drag.position)
+
+                awaitPointerEventScope {
+                    if (
+                        drag(drag.id) {
+                            onDrag(it, it.positionChange())
+                            it.consumePositionChange()
+                        }
+                    ) {
+                        // consume up if we quit drag gracefully with the up
+                        currentEvent.changes.forEach {
+                            if (it.changedToUp()) {
+                                it.consumeDownChange()
+                            }
+                        }
+                        onDragEnd()
+                    } else {
+                        onDragCancel()
+                    }
+                }
+            }
+        } catch (c: CancellationException) {
+            onDragCancel()
+            throw c
+        }
+    }
+}
+
+private suspend fun PointerInputScope.awaitLongPressOrCancellation(
+    initialDown: PointerInputChange
+): PointerInputChange? {
+    var longPress: PointerInputChange? = null
+    var currentDown = initialDown
+    val longPressTimeout = 100L
+    return try {
+        // wait for first tap up or long press
+        withTimeout(longPressTimeout) {
+            awaitPointerEventScope {
+                var finished = false
+                while (!finished) {
+                    val event = awaitPointerEvent(PointerEventPass.Main)
+                    if (event.changes.all { it.changedToUpIgnoreConsumed() }) {
+                        // All pointers are up
+                        finished = true
+                    }
+
+                    if (
+                        event.changes.any { it.consumed.downChange || it.isOutOfBounds(size) }
+                    ) {
+                        finished = true // Canceled
+                    }
+
+                    // Check for cancel by position consumption. We can look on the Final pass of
+                    // the existing pointer event because it comes after the Main pass we checked
+                    // above.
+                    val consumeCheck = awaitPointerEvent(PointerEventPass.Final)
+                    if (consumeCheck.changes.any { it.positionChangeConsumed() }) {
+                        finished = true
+                    }
+                    if (!event.isPointerUp(currentDown.id)) {
+                        longPress = event.changes.firstOrNull { it.id == currentDown.id }
+                    } else {
+                        val newPressed = event.changes.firstOrNull { it.pressed }
+                        if (newPressed != null) {
+                            currentDown = newPressed
+                            longPress = currentDown
+                        } else {
+                            // should technically never happen as we checked it above
+                            finished = true
+                        }
+                    }
+                }
+            }
+        }
+        null
+    } catch (_: TimeoutCancellationException) {
+        longPress ?: initialDown
+    }
+}
+
+private fun PointerEvent.isPointerUp(pointerId: PointerId): Boolean =
+    changes.firstOrNull { it.id == pointerId }?.pressed != true
