26826b34133565a7f422209777a4831de8ac9002
Robert Linsener <rolins@gmx.de>
2022-11-01 00:10:36 +0300

    Updated dependencies, added compose BOM (2022.10.00)

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -76,5 +76,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
 }
